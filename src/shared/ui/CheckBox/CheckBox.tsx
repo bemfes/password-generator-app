b5236cd60a2bc2b9072b9b0aa42a7a3fba418054
Anastasiia Ustin <2anastasiia@mail.ru>
2026-02-10 17:55:46 +0200

    added props to checkbox component

--- src/shared/ui/CheckBox/CheckBox.tsx
+++ src/shared/ui/CheckBox/CheckBox.tsx
@@ -2,10 +2,10 @@ import { FC } from "react";
 import { CheckBoxProps } from "./types";
 import './CheckBox.css'
 
-const CheckBox: FC<CheckBoxProps> = ({ labelText, id, className }) => {
+const CheckBox: FC<CheckBoxProps> = ({ labelText, id, className, onChange, checked }) => {
 
     return <div className={className}>
-        <input id={id} type="checkbox" />
+        <input checked={checked} onChange={onChange} id={id} type="checkbox" />
         <div></div>
         <label htmlFor={id}>{labelText}</label>
     </div>
