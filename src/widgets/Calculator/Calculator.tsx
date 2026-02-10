849bf233338600f173aa50a7ead3d896ab566f5b
Anastasiia Ustin <2anastasiia@mail.ru>
2026-02-10 16:53:29 +0200

    added password placeholder and copy button to calculator widget

--- src/widgets/Calculator/Calculator.tsx
+++ src/widgets/Calculator/Calculator.tsx
@@ -2,10 +2,18 @@ import Button from "../../shared/ui/Button"
 import CheckBox from "../../shared/ui/CheckBox"
 import RangeInput from "../../shared/ui/rangeInput"
 import "./Calculator.css"
+import CopyIcon from '../../shared/assets/copy-icon.png'
 
 const Calculator = () => {
     return <div className="calculator-box">
-        <div className="calculator-content-box"></div>
+        <div className="calculator-content-box">
+            <div className="calculator-password-content">
+                <p><span className="password-placeholder">Password...</span></p>
+                <Button className="" onClick={() => console.log()}>
+                    <img width={30} height={30} src={CopyIcon}/>
+                </Button>
+            </div>
+        </div>
         <div className="calculator-content-box">
             <RangeInput className="range-input-box" labelText="Character length" id='range'/>
             <CheckBox labelText="Include Uppercase Letters" id="uppercase" className="checkbox-container"/>
