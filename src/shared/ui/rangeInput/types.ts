e4fa27fb24689b5a6458796b7924ff1c75f0405b
Anastasiia Ustin <2anastasiia@mail.ru>
2026-02-10 17:33:05 +0200

    added types for range input props

--- src/shared/ui/rangeInput/types.ts
+++ src/shared/ui/rangeInput/types.ts
@@ -1,5 +1,11 @@
+import React from "react";
+
 export interface RangeInputProps {
     labelText: string;
     id: string;
     className: string;
+    min: string;
+    max: string;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
