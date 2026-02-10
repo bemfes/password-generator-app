6e760ccc5f4295b40cb6f7ee02a5d8d64fe3a7ed
Anastasiia Ustin <2anastasiia@mail.ru>
2026-02-10 17:09:39 +0200

    created types for calc-store

---
+++ src/shared/store/types.ts
@@ -0,0 +1,8 @@
+export interface InitialStateTypes {
+    password: null | string;
+    length: number;
+    checkedUpperCase: boolean;
+    checkedLowerCase: boolean;
+    checkedNumbers: boolean;
+    checkedSymbols: boolean;
+}
