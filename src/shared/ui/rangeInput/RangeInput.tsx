aff38ac20ba09da5cf9bc3498a8e9683d43c92eb
Anastasiia Ustin <2anastasiia@mail.ru>
2026-02-10 19:07:34 +0200

    added code for dynamically changing range input's background in range input component

--- src/shared/ui/rangeInput/RangeInput.tsx
+++ src/shared/ui/rangeInput/RangeInput.tsx
@@ -3,12 +3,25 @@ import { RangeInputProps } from "./types";
 import "./RangeInput.css"
 
 const RangeInput: FC<RangeInputProps> = ({ labelText, id, className, min, max, value, onChange }) => {
+    
+  const percentage = ((Number(value) - Number(min)) / (Number(max)- Number(min))) * 100;
+
+  const backgroundStyle = {
+    background: `linear-gradient(
+      to right,
+      #A4FFAF 0%, 
+      #A4FFAF ${percentage}%, 
+      #18171F ${percentage}%, 
+      #18171F 100%
+    )`,
+  };
+
     return <div className={className}>
         <div>
             <label htmlFor={id}>{labelText}</label>
             <p>{value}</p>
         </div>
-        <input onChange={onChange} min={min} max={max} value={value} id={id} type="range" />
+        <input style={backgroundStyle} onChange={onChange} min={min} max={max} value={value} id={id} type="range" />
 
     </div>
 }
