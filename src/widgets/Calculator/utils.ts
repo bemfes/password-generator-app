f596b519ccbeb17116ab774dc6d288dceb6b2b3d
Anastasiia Ustin <2anastasiia@mail.ru>
2026-02-11 13:28:43 +0200

    added utils file with function for generating password

---
+++ src/widgets/Calculator/utils.ts
@@ -0,0 +1,40 @@
+import useCalcStore from "../../shared/store";
+
+export function generatePassword(): void {
+
+    const checkedUpperCase = useCalcStore.getState().checkedUpperCase
+    const checkedLowerCase = useCalcStore.getState().checkedLowerCase
+    const checkedNumbers = useCalcStore.getState().checkedNumbers
+    const checkedSymbols = useCalcStore.getState().checkedSymbols
+    const length = Number(useCalcStore.getState().length)
+
+    let password: string = ''
+
+    let symbols: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!№;%:?*()_+=";
+
+    for (let i = 0; i < length; i++) {
+        if (!checkedNumbers) {
+           symbols = (symbols.match(/\D/gi))!.join('')
+           
+        }
+        if (!checkedSymbols) {
+            symbols = (symbols.match(/[A-Za-z0-9]/gi))!.join('')
+        }
+        if (!checkedLowerCase) {
+            symbols = (symbols.match(/[^a-z]/g))!.join('')
+            
+        }
+        if (!checkedUpperCase) {
+            symbols = (symbols.match(/[^A-Z]/g))!.join('')
+            
+        }
+        
+        const random = symbols.charAt(Math.floor(Math.random() * symbols.length)) 
+        
+        password += random
+
+    }
+
+    useCalcStore.getState().setPassword(password)
+
+}
