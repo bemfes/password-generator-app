96f2374fcc7d05d1ab5eaf17a229b6db39b77dd0
Anastasiia Ustin <2anastasiia@mail.ru>
2026-02-23 21:36:42 +0200

    set alias for src path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr';
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
-  base: '/password-generator-app/'
+  base: '/password-generator-app/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
