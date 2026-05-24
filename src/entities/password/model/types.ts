c40db1ef60d3ce68f2e2b55d08eb2551194a38bf
Anastasiia Ustin <2anastasiia@mail.ru>
2026-05-24 11:05:32 +0200

    refactor: move store to entity password

---
+++ src/entities/password/model/types.ts
@@ -0,0 +1,17 @@
+export interface InitialStateTypes {
+  password: null | string;
+  length: string;
+  checkedUpperCase: boolean;
+  checkedLowerCase: boolean;
+  checkedNumbers: boolean;
+  checkedSymbols: boolean;
+}
+
+export interface UseCalcStoreTypes extends InitialStateTypes {
+  changeUpperCase: () => void;
+  changeLowerCase: () => void;
+  changeNumbers: () => void;
+  changeSymbols: () => void;
+  changeLength: (value: string) => void;
+  setPassword: (value: string) => void;
+}
