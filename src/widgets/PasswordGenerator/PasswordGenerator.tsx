0d1997208bb49c6d8183fbcbc61f426a6821405e
Anastasiia Ustin <2anastasiia@mail.ru>
2026-05-24 12:41:42 +0200

    refactor: add PasswordGenerationButton

--- src/widgets/PasswordGenerator/PasswordGenerator.tsx
+++ src/widgets/PasswordGenerator/PasswordGenerator.tsx
@@ -1,29 +1,13 @@
-import Button from "@/shared/ui/Button";
 import "./PasswordGenerator.css";
 import usePasswordStore from "@/entities/password/model";
-import { generatePassword } from "./utils";
 import IconCopy from "@/shared/assets/icon-copy.svg?react";
 import ShowStrength from "@/features/show-strength/showStrength";
 import CopyTextButton from "@/features/copy-text/ui";
 import PasswordSettings from "@/features/password-settings/ui";
+import GeneratePasswordButton from "@/features/generate-password/ui";
 
 const PasswordGenerator = () => {
-  const checkedUpperCase = usePasswordStore((state) => state.checkedUpperCase);
-
-  const checkedLowerCase = usePasswordStore((state) => state.checkedLowerCase);
-
-  const checkedNumbers = usePasswordStore((state) => state.checkedNumbers);
-
-  const checkedSymbols = usePasswordStore((state) => state.checkedSymbols);
-
   const password = usePasswordStore((state) => state.password);
-
-  const generateBtnAccess = [
-    checkedUpperCase,
-    checkedLowerCase,
-    checkedNumbers,
-    checkedSymbols,
-  ];
 
   return (
     <div className="calculator-box">
@@ -52,13 +36,7 @@ const PasswordGenerator = () => {
       <div className="calculator-content-box">
         <PasswordSettings />
         <ShowStrength />
-        <Button
-          disabled={generateBtnAccess.includes(true) ? false : true}
-          className="button green-button"
-          onClick={generatePassword}
-        >
-          GENERATE
-        </Button>
+        <GeneratePasswordButton />
       </div>
     </div>
   );
