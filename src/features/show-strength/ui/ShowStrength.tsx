c8758798b0d3cec9a0cb262941f605e2e06c2275
Anastasiia Ustin <2anastasiia@mail.ru>
2026-05-24 16:36:39 +0200

    refactor: add useShowStrength

--- src/features/show-strength/ui/ShowStrength.tsx
+++ src/features/show-strength/ui/ShowStrength.tsx
@@ -1,33 +1,15 @@
 import { FC } from "react";
 import "./ShowStrength.css";
-import { countStrength, strengthLevels } from "../utils";
-import usePasswordStore from "@/entities/password/model";
+import { useShowStrength } from "../model";
 
 const ShowStrength: FC = () => {
-  const checkedUpperCase = usePasswordStore((state) => state.checkedUpperCase);
-
-  const checkedLowerCase = usePasswordStore((state) => state.checkedLowerCase);
-
-  const checkedNumbers = usePasswordStore((state) => state.checkedNumbers);
-
-  const checkedSymbols = usePasswordStore((state) => state.checkedSymbols);
-
-  const length = usePasswordStore((state) => state.length);
-
-  const checkedProperties = [
-    checkedUpperCase,
-    checkedLowerCase,
-    checkedNumbers,
-    checkedSymbols,
-  ];
-
-  const strength = countStrength(checkedProperties, length);
+  const { strength, strengthLevel } = useShowStrength();
 
   return (
     <div className="show-strength-box">
       <p className="show-strength-title">STRENGTH</p>
       <div className="show-strength-right">
-        <p className="show-strength-name">{strengthLevels[strength]}</p>
+        <p className="show-strength-name">{strengthLevel}</p>
         <div className={`show-strength-cols show-strength-cols-${strength}`}>
           <div className="show-strength-col"></div>
           <div className="show-strength-col"></div>
