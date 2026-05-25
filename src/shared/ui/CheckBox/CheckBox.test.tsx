f34a4a80194f8b45874a464c4e24cdfb7ddf6c2b
Anastasiia Ustin <2anastasiia@mail.ru>
2026-05-25 20:01:56 +0200

    refactor: move render checkbox to hepler function

--- src/shared/ui/CheckBox/CheckBox.test.tsx
+++ src/shared/ui/CheckBox/CheckBox.test.tsx
@@ -1,80 +1,45 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import CheckBox from "./CheckBox";
 
+const renderCheckBox = (props = {}) => {
+  render(
+    <CheckBox
+      onChange={vi.fn()}
+      checked
+      labelText="Choose age"
+      className="green"
+      id="age"
+      {...props}
+    />,
+  );
+};
+
 describe("CheckBox", () => {
   it("should render checkbox", () => {
-    render(
-      <CheckBox
-        onChange={vi.fn()}
-        checked
-        labelText="Choose age"
-        className="green"
-        id="age"
-      />,
-    );
+    renderCheckBox();
     expect(screen.getByRole("checkbox")).toBeInTheDocument();
   });
   it("should have label", () => {
-    render(
-      <CheckBox
-        onChange={vi.fn()}
-        checked
-        labelText="Choose age"
-        className="green"
-        id="age"
-      />,
-    );
+    renderCheckBox();
     expect(screen.getByLabelText(/Choose age/i)).toBeInTheDocument();
   });
   it("should invoke onChange when user clicks checkbox", () => {
     const onChange = vi.fn();
-    render(
-      <CheckBox
-        onChange={onChange}
-        checked
-        labelText="Choose age"
-        className="green"
-        id="age"
-      />,
-    );
+    renderCheckBox({ onChange });
     const elem = screen.getByRole("checkbox");
     fireEvent.click(elem);
     expect(onChange).toHaveBeenCalledOnce();
   });
   it("should be checked when checked prop is true", () => {
-    render(
-      <CheckBox
-        onChange={vi.fn()}
-        checked
-        labelText="Choose age"
-        className="green"
-        id="age"
-      />,
-    );
+    renderCheckBox();
     expect(screen.getByRole("checkbox")).toBeChecked();
   });
   it("should not be checked when checked prop is false", () => {
-    render(
-      <CheckBox
-        onChange={vi.fn()}
-        checked={false}
-        labelText="Choose age"
-        className="green"
-        id="age"
-      />,
-    );
+    renderCheckBox({ checked: false });
     expect(screen.getByRole("checkbox")).not.toBeChecked();
   });
   it("should have classname", () => {
-    render(
-      <CheckBox
-        onChange={vi.fn()}
-        checked
-        labelText="Choose age"
-        className="green"
-        id="age"
-      />,
-    );
+    renderCheckBox();
     expect(screen.getByRole("checkbox").parentElement).toHaveClass("green");
   });
 });
