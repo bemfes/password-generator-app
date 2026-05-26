874412a803d6c55d9e4b180ff0962b8dd8b8df95
Anastasiia Ustin <2anastasiia@mail.ru>
2026-05-26 12:26:45 +0200

    refactor: create variable for Number(length)

--- src/features/show-strength/lib/countStrength.ts
+++ src/features/show-strength/lib/countStrength.ts
@@ -3,23 +3,26 @@ export function countStrength(
   length: string,
 ): number {
   const checkedPropertiesLength = checkedProperties.filter(Boolean).length;
+
+  const passwordLength = Number(length);
+
   if (checkedPropertiesLength === 1) {
     return 1;
   }
   if (
     (checkedPropertiesLength === 2 || checkedPropertiesLength === 3) &&
-    Number(length) < 10
+    passwordLength < 10
   ) {
     return 2;
   }
   if (
     ((checkedPropertiesLength === 3 || checkedPropertiesLength === 2) &&
-      Number(length) >= 10) ||
-    (checkedPropertiesLength === 4 && Number(length) < 10)
+      passwordLength >= 10) ||
+    (checkedPropertiesLength === 4 && passwordLength < 10)
   ) {
     return 3;
   }
-  if (checkedPropertiesLength === 4 && Number(length) >= 10) {
+  if (checkedPropertiesLength === 4 && passwordLength >= 10) {
     return 4;
   }
   return 0;
