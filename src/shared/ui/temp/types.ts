6d6a7a7f9a169f9d325836d170d1e459c8906b73
Anastasiia Ustin <2anastasiia@mail.ru>
2026-05-27 11:59:22 +0200

    fix: rename rangeInput to temp

---
+++ src/shared/ui/temp/types.ts
@@ -0,0 +1,13 @@
+import React from "react";
+
+export interface RangeInputProps {
+  labelText: string;
+  id: string;
+  className: string;
+  min: string;
+  max: string;
+  value: string;
+  fullTrackColor: string;
+  emptyTrackColor: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
